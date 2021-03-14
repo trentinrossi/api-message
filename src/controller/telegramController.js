2d33e70a74b3ea960bf15b87ebae947bb9431e90
Rodrigo Rossi <rdrg.rossi@gmail.com>
2021-03-13 22:05:52 -0300

    Commit for backup

--- src/controller/telegramController.js
+++ src/controller/telegramController.js
@@ -1,5 +1,16 @@
+const { TelegramClient } = require('messaging-api-telegram');
+
 function sendMessage(req, res) {
-  return res.status(200).json("OK"); 
+  const client = new TelegramClient({
+    accessToken: '1575648477:AAHR0-3pjeTU7M4KVBuGYs3V8y9vkbT46fI',
+  });
+
+  await client.sendMessage(CHAT_ID, 'hi', {
+    disableWebPagePreview: true,
+    disableNotification: true,
+  });
+
+  return res.status(200).json('OK');
 }
 
 module.exports = {
